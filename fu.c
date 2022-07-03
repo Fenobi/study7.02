38a28dfa5ce6f8546f840c4133b39de88d747d0a
Fenobi <13667739122@qq.com>
2022-07-03 18:58:34 +0800

    添加项目文件。

---
+++ fu.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include <stdio.h>
+//#include <assert.h>
+//
+//int my_strlen(const char* str)
+//{
+//	assert(str);
+//
+//	int count = 0;
+//	while (*str++)
+//	{
+//		count++;
+//	}
+//	return count;
+//}
+//
+//int main()
+//{
+//	char arr[] = "abcde";
+//	printf("%d\n", my_strlen(arr));
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <assert.h>
+//
+//char* my_strcpy(char* dest, const char* src)
+//{
+//	assert(src);
+//
+//	char* ret = dest;
+//	while (*dest++ = *src++);
+//	return ret;
+//}
+//
+//int main()
+//{
+//	char arr[] = "abcd";
+//	printf("%s\n", my_strcpy(arr,"ABCD"));
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <assert.h>
+//
+//char* my_strcat(char* dest, const char* src)
+//{
+//	assert(dest && src);
+//
+//	char* ret = dest;
+//	while (*dest)
+//	{
+//		dest++;
+//	}
+//	while (*dest++ = *src++);
+//	return ret;
+//}
+//
+//int main()
+//{
+//	char arr[8] = "abcd";
+//	printf("%s\n", my_strcat(arr, "efg"));
+//	return 0;
+//}
+
